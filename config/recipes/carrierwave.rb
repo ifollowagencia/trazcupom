58b9c4e8cdfed5ca33c1f23e652dfd38a1cd73dd
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2014-02-02 23:09:05 -0300

    conf

--- config/recipes/carrierwave.rb
+++ config/recipes/carrierwave.rb
@@ -1,13 +1,1 @@
-namespace :carrierwave do 
-  task :uploads_folder do
-    run "mkdir -p #{deploy_to}/#{shared_dir}/uploads"
-    run "#{sudo} chmod 775 #{deploy_to}/#{shared_dir}/uploads"
-  end
-  after 'deploy:setup', 'carrierwave:uploads_folderx'
-
-  task :symlink do 
-    #run "ln -nfs #{deploy_to}/#{shared_dir}/uploads #{release_path}/public/uploads"
-    run "ln -nfs #{deploy_to}/#{shared_dir}/public/uploads #{release_path}/public/uploads"
-  end
-  after 'deploy', 'carrierwave:symlink'
-end
+# Acho que agora funcionou
