4a95bab009990c0f51347696b130fa8c2473099a
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-02-03 21:34:25 -0300

    - first implementation to suggest offers to the user

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,15 +1,22 @@
 class Offer < ActiveRecord::Base
 
-
-  
-  
-
-  mount_uploader :image1, EstablishmentUploader	
-  mount_uploader :image2, EstablishmentUploader	
-  mount_uploader :image3, EstablishmentUploader	
+  mount_uploader :image1, EstablishmentUploader
+  mount_uploader :image2, EstablishmentUploader
+  mount_uploader :image3, EstablishmentUploader
   mount_uploader :image4, EstablishmentUploader
 
   belongs_to :establishment
   belongs_to :product
+
+  has_many :visited_offers
+
+  def self.suggestions_by_category_establishments(current_user)
+    offers = []
+    category_ids = current_user.visited_offers.group(:category_establishments_id).order("count_all DESC").count.keys
+    category_ids.each do |category_id|
+      offers.concat(includes(:establishment).where("establishments.categoryestablishment_id = ?", category_id))
+    end
+    offers
+  end
 
 end
