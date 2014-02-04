0da0e95b85ea382d586a07bdd305b6211fb68276
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-02-03 21:37:03 -0300

    - add suggestions based on offers visit - code identation - small refatoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+require 'will_paginate/array'
+
 class HomeController < ApplicationController
 
 helper :home
@@ -7,69 +9,68 @@ WillPaginateRenderers.pagination_options[:twitter_label] = "Carregar mais"
 WillPaginateRenderers.pagination_options[:twitter_class] = 'buttom_twitter'
    # GET /offers
   # GET /offers.xml
-  def nearme 
-    @city = params[:city_id]  
+  def nearme
+    @city = params[:city_id]
     session[:city] = @city
 
-  	if user_signed_in?
-  		#pegar dados do usuario logado
-     
-    puts "Cidade: #{@city}"
-  	@name = current_user.name
-    @estabs = estabs
-    
+    if user_signed_in?
+      #pegar dados do usuario logado
 
-    resultoffer = Offer.all
-    
+      puts "Cidade: #{@city}"
+      @name = current_user.name
+      @estabs = estabs
 
-    sleep 1
-    
-    @cupons = resultoffer.paginate(:page=>params[:page], :per_page => 6)
+      sleep 1
 
-    @pins = Addressestablishment.all
-    @hash = Gmaps4rails.build_markers(@pins) do |pin, marker|
-    marker.lat pin.latitude
-    marker.lng pin.longitude
-    marker.picture({
-    "picture" => "/home/images/location.png",
-     "width" =>  '32',        
-     "height" => '32'
-     })
+      @cupons = offer_suggestions.paginate(page: params[:page], per_page: 6)
 
+      @pins = Addressestablishment.all
+      build_makers_for_map
+    else
+      # caso não esteja logado redireciona pro login
+      redirect_to new_user_session_path
     end
 
-
-  	else
-      # caso não esteja logado redireciona pro login 	   	
-  		redirect_to new_user_session_path
-
-  	end
-  	
   end  #final do método nearme
 
   def gmaps4rails_marker_picture
     {
-    "picture" => "http://www.trazcupom.com/home/images/location.png",
-    "width" => 32, #beware to resize your pictures properly
-    "height" => 32 #beware to resize your pictures properly
+      "picture" => "http://www.trazcupom.com/home/images/location.png",
+      "width" => 32, #beware to resize your pictures properly
+      "height" => 32 #beware to resize your pictures properly
     }
-end
+  end
 
   def estabs
-    
     @estabs = Establishment.find(:all, :order => "id DESC", :limit => 16).reverse
-
   end
 
   def cuponsrecentes
-    
-    @cuponsrecentes = Offer.find(:all, :order => "validity DESC", :limit => 9).reverse
-
+    @cuponsrecentes = Offer.all.order(validity: :desc).limit(9).reverse
+    #@cuponsrecentes = Offer.find(:all, :order => "validity DESC", :limit => 9).reverse
   end
 
+  private
 
+  def offer_suggestions
+    if current_user.visited_offers.empty?
+      Offer.all
+    else
+      Offer.suggestions_by_category_establishments(current_user)
+    end
+  end
 
-
-
+  def build_makers_for_map
+    @pins = Addressestablishment.all
+    @hash = Gmaps4rails.build_markers(@pins) do |pin, marker|
+      marker.lat pin.latitude
+      marker.lng pin.longitude
+      marker.picture({
+        "picture" => "/home/images/location.png",
+        "width" =>  '32',
+        "height" => '32'
+      })
+    end
+  end
 
 end
