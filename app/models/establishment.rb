13f665de543fa5ae7ae9da339083b41a3c4aa8b8
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-02-03 22:11:21 -0300

    bug fix: add categoryestablishment to establishment

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -1,8 +1,12 @@
 class Establishment < ActiveRecord::Base
   belongs_to :city
   belongs_to :plan
+  belongs_to :establishment
+
   has_many :offers
   has_many :products
+
   validates :email, uniqueness: true
-  mount_uploader :imageprofile, EstablishmentUploader	
+  mount_uploader :imageprofile, EstablishmentUploader
+
 end
