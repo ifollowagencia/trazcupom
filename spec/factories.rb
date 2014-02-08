474628eb4f1d3dd485528c5f34725ef4bb364b1c
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-02-07 21:58:32 -0300

    added address_establishment and establishhment factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,26 @@ FactoryGirl.define do
      sequence(:email) {|n| "user#{n}@email.com" }
      password '12345678'
    end
+   factory :address_establishment do
+     address 'Rua Dom Aquino, 2045 Sala 04'
+     latitude  -20.4603
+     longitude -20.4603
+   end
+
+   factory :establishment do
+     name 'Establishment'
+     surname 'Establishment Surname'
+     phone '(55) 87878787'
+     sequence(:email) {|n| "establishment#{n}@email.com" }
+     password '123456'
+     cnpj  '123456'
+     reason 'Ifollow'
+     city_id 1
+     plan_id 1
+     imageprofile 'logo.png'
+     association :address_establishment
+   end
+
 
    factory :product do
      name "NAME"
