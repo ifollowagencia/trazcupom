6bbbf135fd62e0876cb340757b9b2c589f64df57
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-02-07 21:23:36 -0300

    ruby > 1.9 syntax

--- app/models/address_establishment.rb
+++ app/models/address_establishment.rb
@@ -1,6 +1,6 @@
 class AddressEstablishment < ActiveRecord::Base
   belongs_to :establishment
   geocoded_by :address
-  after_validation :geocode, :if => :address_changed?
+  after_validation :geocode, if: :address_changed?
 
 end
