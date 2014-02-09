9ae5f8a03f3a7bcf4f14df2c890fb4c828f4feca
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-02-09 17:13:22 -0300

    add more tests to offers controllers and make all tests pass

--- spec/controllers/offers_controller_spec.rb
+++ spec/controllers/offers_controller_spec.rb
@@ -7,9 +7,57 @@ describe OffersController do
     sign_in user
   end
 
-  it 'show offerts' do
-    offer = create(:offer)
-    get :show, id: offer
-    expect(assigns(:offer)).to eq offer
+
+  describe 'GET index' do
+    it 'populates an array' do
+      offers = create(:offer)
+      get :index
+      expect(assigns(:offers)).to eq [offers]
+    end
+
+    it 'render the :index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET new' do
+    it "assigns a new AddressEstablishmet to @offer" do
+      get :new
+      expect(assigns(:offer)).to be_a_new(Offer)
+    end
+
+    it "render the new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET edit' do
+    it "assigns the requested AddressEstablishmet to @offer" do
+      offer = create(:offer)
+      get :edit, id: offer
+      expect(assigns(:offer)).to eq offer
+    end
+
+    it "render the new template" do
+      offer = create(:offer)
+      get :edit, id: offer
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'GET show' do
+    it 'show offerts' do
+      offer = create(:offer)
+      get :show, id: offer
+      expect(assigns(:offer)).to eq offer
+    end
+
+    it "render the show template" do
+      offer = create(:offer)
+      get :show, id: offer
+      expect(response).to render_template :show
+    end
   end
 end
