93853fd2a31b2665878938dd9243b7fd5f052cb0
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-02-13 16:18:30 -0300

    refactor: change spec name

--- spec/api/resources/users_spec.rb
+++ spec/api/resources/users_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Resources::Users do
+
   let(:user) { create(:user) }
-  it "valid route" do
+
+  it "perform login" do
     post '/api/users/login', user: { email: user.email, password: user.password }
     expect(response.code).to eq "200"
     expect(JSON.parse(response.body)).to include('authentication_token')
