dc8024f194b20e5281e77a5b4eeab7f17cf14caa
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-02-12 22:59:53 -0300

    load path of api

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,12 @@ module Trazcupom
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.autoload_paths += %W( #{config.root}/lib/utilities )
+    config.paths.add "app/api", glob: "**/*.rb"
+    config.autoload_paths += Dir["#{Rails.root}/app/api/*"]
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
-          
+    config.assets.initialize_on_precompile = true
+
   end
 end
